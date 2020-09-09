9733a441782fe56ee3fe722729ed98b591d9655d
Stareven233 <1429960342@qq.com>
2020-09-09 23:36:32 +0800

    refactor(GHAnalysis): simplify code logic

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -1,141 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+初始化：
+    input: python GHAnalysis.py -i data
+    output: 0
+测试：
+    input: python GHAnalysis.py -e PushEvent -u greatfire
+    output: 24
+
+EventName
+    PushEvent
+    IssueCommentEvent
+    IssuesEvent
+    PullRequestEvent
+"""
+
 import json
 import os
 import argparse
+# import pickle
+# todo 替换下方的文件读写
+
+# todo 为getEventsUsers等三个函数改名，并优化Run.analyse即可
 
 
 class Data:
-    def __init__(self, dict_address: str = None, reload: int = 0):
-        if reload == 1:
-            self.__init(dict_address)
-        if dict_address is None and not os.path.exists('1.json') and not os.path.exists('2.json') and not os.path.exists('3.json'):
-            # todo any()
-            raise RuntimeError('error: init failed')
-        x = open('1.json', 'r', encoding='utf-8').read()
-        self.__4Events4PerP = json.loads(x)
-        x = open('2.json', 'r', encoding='utf-8').read()
-        self.__4Events4PerR = json.loads(x)
-        x = open('3.json', 'r', encoding='utf-8').read()
-        self.__4Events4PerPPerR = json.loads(x)
+    def __init__(self):
+        self.__user_events = {}
+        self.__repo_events = {}
+        self.__user_repo_events = {}
 
-    def __init(self, dict_address: str):
-        json_list = []
-        for root, dic, files in os.walk(dict_address):
-            for f in files:
-                if f[-5:] == '.json':
-                    json_path = f
-                    x = open(dict_address+'\\'+json_path,
-                             'r', encoding='utf-8').read()
-                    str_list = [_x for _x in x.split('\n') if len(_x) > 0]
-                    for i, _str in enumerate(str_list):
-                        try:
-                            json_list.append(json.loads(_str))
-                        except:
-                            pass
-        records = self.__listOfNestedDict2ListOfDict(json_list)
-        self.__4Events4PerP = {}
-        self.__4Events4PerR = {}
-        self.__4Events4PerPPerR = {}
-        for i in records:
-            if not self.__4Events4PerP.get(i['actor__login'], 0):
-                self.__4Events4PerP.update({i['actor__login']: {}})
-                self.__4Events4PerPPerR.update({i['actor__login']: {}})
-            self.__4Events4PerP[i['actor__login']][i['type']
-                                         ] = self.__4Events4PerP[i['actor__login']].get(i['type'], 0)+1
-            if not self.__4Events4PerR.get(i['repo__name'], 0):
-                self.__4Events4PerR.update({i['repo__name']: {}})
-            # todo setdefault
-            self.__4Events4PerR[i['repo__name']][i['type']
-                                       ] = self.__4Events4PerR[i['repo__name']].get(i['type'], 0)+1
-            if not self.__4Events4PerPPerR[i['actor__login']].get(i['repo__name'], 0):
-                self.__4Events4PerPPerR[i['actor__login']].update({i['repo__name']: {}})
-            self.__4Events4PerPPerR[i['actor__login']][i['repo__name']][i['type']
-                                                          ] = self.__4Events4PerPPerR[i['actor__login']][i['repo__name']].get(i['type'], 0)+1
-        with open('1.json', 'w', encoding='utf-8') as f:
-            json.dump(self.__4Events4PerP,f)
-        with open('2.json', 'w', encoding='utf-8') as f:
-            json.dump(self.__4Events4PerR,f)
-        with open('3.json', 'w', encoding='utf-8') as f:
-            json.dump(self.__4Events4PerPPerR,f)
-
-    def __parseDict(self, d: dict, prefix: str):
-        # 将字典递归降维，prefix只由当前及上层的key构成
-        # {'a': {'b': {'c': {'d': 'e'}}, 'bb': 'bb'}, 'aa': {'aa': 2}} -> {'c__d': 'e', 'a__bb': 'bb', 'aa__aa': 2}
-        _d = {}
-        for k in d.keys():
-            if str(type(d[k]))[-6:-2] == 'dict':
-                # type(d[k]) == dict
-                _d.update(self.__parseDict(d[k], k))
-            else:
-                _k = f'{prefix}__{k}' if prefix != '' else k
-                _d[_k] = d[k]
-        return _d
-
-    def __listOfNestedDict2ListOfDict(self, a: list):
+    @staticmethod
+    def __read(file_path: str):
         records = []
-        for d in a:
-            _d = self.__parseDict(d, '')
-            records.append(_d)
+        with open(file_path, 'r', encoding='utf-8') as f:
+            for line in f:
+                records.append(json.loads(line))
         return records
 
-    def getEventsUsers(self, username: str, event: str) -> int:
-        if not self.__4Events4PerP.get(username,0):
-            return 0
-        else:
-            return self.__4Events4PerP[username].get(event,0)
+    def init(self, dir_path: str):
+        records = []
+        for cur_dir, sub_dir, filenames in os.walk(dir_path):
+            filenames = filter(lambda r: r.endswith('.json'), filenames)
+            for name in filenames:
+                records.extend(self.__read(f'{dir_path}/{name}'))
 
-    def getEventsRepos(self, reponame: str, event: str) -> int:
-        if not self.__4Events4PerR.get(reponame,0):
-            return 0
-        else:
-            return self.__4Events4PerR[reponame].get(event,0)
+        records = self.__reduce_dicts(records)
 
-    def getEventsUsersAndRepos(self, username: str, reponame: str, event: str) -> int:
-        if not self.__4Events4PerP.get(username,0):
-            return 0
-        elif not self.__4Events4PerPPerR[username].get(reponame,0):
-            return 0
-        else:
-            return self.__4Events4PerPPerR[username][reponame].get(event,0)
+        for i in records:
+            user = i['actor__login']
+            repo = i['repo__name']
+            event = i['type']
+
+            self.__user_events.setdefault(user, {})
+            self.__user_repo_events.setdefault(user, {})
+            self.__repo_events.setdefault(repo, {})
+            self.__user_repo_events[user].setdefault(repo, {})
+
+            self.__user_events[user][event] = self.__user_events[user].get(event, 0)+1
+            self.__repo_events[repo][event] = self.__repo_events[repo].get(event, 0)+1
+            self.__user_repo_events[user][repo][event] = self.__user_repo_events[user][repo].get(event, 0)+1
+
+        with open('0.json', 'w', encoding='utf-8') as f:
+            json.dump(self.__user_events, f)
+        with open('1.json', 'w', encoding='utf-8') as f:
+            json.dump(self.__repo_events, f)
+        with open('2.json', 'w', encoding='utf-8') as f:
+            json.dump(self.__user_repo_events, f)
+
+    def load(self):
+        if not any((os.path.exists(f'{i}.json') for i in range(3))):
+            raise RuntimeError('error: data file not found')
+
+        x = open('0.json', 'r', encoding='utf-8').read()
+        self.__user_events = json.loads(x)
+        x = open('1.json', 'r', encoding='utf-8').read()
+        self.__repo_events = json.loads(x)
+        x = open('2.json', 'r', encoding='utf-8').read()
+        self.__user_repo_events = json.loads(x)
+
+    def __reduce_one(self, d: dict, prefix: str):
+        """
+        将字典递归降维，prefix只由当前及上层的key构成
+        {'a': {'b': {'c': {'d': 'e'}}, 'bb': 'bb'}, 'aa': {'aa': 2}} -> {'c__d': 'e', 'a__bb': 'bb', 'aa__aa': 2}
+        """
+
+        # todo 或许可直接d[actor][login]取出想要的三个返回
+        dd = {}
+        for k, v in d.items():
+            if isinstance(v, dict):
+                dd.update(self.__reduce_one(v, k))
+            else:
+                dd[f'{prefix}__{k}' if prefix != '' else k] = v
+        return dd
+
+    def __reduce_dicts(self, records_list: list):
+        return [self.__reduce_one(d, '') for d in records_list]
+
+    def get_user_event(self, user: str, event: str) -> int:
+        return self.__user_events.get(user, {}).get(event, 0)
+
+    def get_repo_event(self, repo: str, event: str) -> int:
+        return self.__repo_events.get(repo, {}).get(event, 0)
+
+    def get_user_repo_event(self, user: str, repo: str, event: str) -> int:
+        return self.__user_repo_events.get(user, {}).get(repo, {}).get(event, 0)
 
 
 class Run:
     def __init__(self):
+        self.basedir = os.path.dirname(os.path.abspath(__file__))
         self.parser = argparse.ArgumentParser()
         self.data = None
-        self.argInit()
-        print(self.analyse())
+        self.arg_init()
 
-    def argInit(self):
-        self.parser.add_argument('-i', '--init')
-        self.parser.add_argument('-u', '--user')
-        self.parser.add_argument('-r', '--repo')
-        self.parser.add_argument('-e', '--event')
+    def arg_init(self):
+        self.parser.add_argument('-i', '--init', type=str)
+        self.parser.add_argument('-u', '--user', type=str)
+        self.parser.add_argument('-r', '--repo', type=str)
+        self.parser.add_argument('-e', '--event', type=str)
 
     def analyse(self):
-        if self.parser.parse_args().init:
-            self.data = Data(self.parser.parse_args().init, 1)
-            return 0
+        args = self.parser.parse_args()
+
+        self.data = Data()
+        if args.init:
+            self.data.init(args.init)
         else:
-            if self.data is None:
-                self.data = Data()
-            if self.parser.parse_args().event:
-                if self.parser.parse_args().user:
-                    if self.parser.parse_args().repo:
-                        # 查询某个用户在某个项目中某个事件的数量
-                        res = self.data.getEventsUsersAndRepos(
-                            self.parser.parse_args().user, self.parser.parse_args().repo, self.parser.parse_args().event)
-                    else:
-                        res = self.data.getEventsUsers(
-                            self.parser.parse_args().user, self.parser.parse_args().event)
-                elif self.parser.parse_args().repo:
-                    res = self.data.getEventsRepos(
-                        self.parser.parse_args().reop, self.parser.parse_args().event)
-                else:
-                    raise RuntimeError('error: argument -l or -c are required')
-            else:
-                raise RuntimeError('error: argument -e is required')
+            self.data.load()
+
+        if not args.event:
+            raise RuntimeError('error: the following arguments are required: -e/--event')
+        if not args.user and not args.repo:
+            raise RuntimeError('error: the following arguments are required: -u/--user or -r/--repo')
+
+        if args.user and args.repo:
+            res = self.data.get_user_repo_event(args.user, args.repo, args.event)
+        elif args.user:
+            res = self.data.get_user_event(args.user, args.event)
+        else:
+            res = self.data.get_repo_event(args.repo, args.event)
         return res
 
 
 if __name__ == '__main__':
     a = Run()
+    print(a.analyse())

25b170aceba9ca5b933aac54bf049679c0e4998b
Stareven233 <1429960342@qq.com>
2020-09-15 16:38:07 +0800

    feat(GHAnalysis.py): use aiofiles for async io

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -17,14 +17,14 @@ import os
 import argparse
 import pickle
 import re
+import asyncio
+import aiofiles
 
-# todo 一边re一边计数
-# todo 尝试去掉多个get_函数
+# todo 尝试aiofiles
+# todo 使用多进程配合协程
 
 EVENTS = ("PushEvent", "IssueCommentEvent", "IssuesEvent", "PullRequestEvent", )
 pattern = re.compile(r'"type":"(\w+?)".*?actor.*?"login":"(\S+?)".*?repo.*?"name":"(\S+?)"')
-# pattern = re.compile('.*?((?:%s)Event).*?actor.*?"login":"(\\S+?)".*?repo.*?"name":"(\\S+?)"' % '|'.join(EVENTS))
-# 正则太长?使用了|多次?会变得特别慢
 
 
 class Data:
@@ -33,26 +33,13 @@ class Data:
         self.repo_events = {}
         self.user_repo_events = {}
 
-    # @staticmethod
-    # def __parse(file_path: str):
-    #     """
-    #     从json文件中逐行抽取所需信息元组(event, user, repo)
-    #     """
-    #     records = []
-    #     with open(file_path, 'r', encoding='utf-8') as f:
-    #         for line in f:
-    #             res = pattern.search(line)
-    #             if res is None or res[1] not in EVENTS:
-    #                 continue
-    #             records.append(res.groups())
-    #     return records
-
-    def __count_events(self, file_path: str):
+    async def __count_events(self, filename: str):
         """
-        从json文件中逐行抽取所需信息元组(event, user, repo)，并写入字典
+        从单个json文件中逐行抽取所需信息元组(event, user, repo)，并写入字典
         """
-        with open(file_path, 'r', encoding='utf-8') as f:
-            for line in f:
+        # with open(filename, 'r', encoding='utf-8') as f:
+        async with aiofiles.open(filename, 'r', encoding='utf-8') as f:
+            async for line in f:
                 res = pattern.search(line)
                 if res is None or res[1] not in EVENTS:
                     continue
@@ -67,25 +54,17 @@ class Data:
                 self.repo_events[repo][event] = self.repo_events[repo].get(event, 0)+1
                 self.user_repo_events[user][repo][event] = self.user_repo_events[user][repo].get(event, 0)+1
 
+    async def __read_files(self, path, filenames):
+        await asyncio.gather(
+            *(self.__count_events(f'{path}/{f}') for f in filenames)
+        )
+
     def init(self, dir_path: str):
-        # records = []
         for cur_dir, sub_dir, filenames in os.walk(dir_path):
             filenames = filter(lambda r: r.endswith('.json'), filenames)
-            for name in filenames:
-                # records.extend(self.__parse(f'{cur_dir}/{name}'))
-                self.__count_events(f'{cur_dir}/{name}')
-
-        # for record in records:
-            # event, user, repo = record
-            #
-            # self.user_events.setdefault(user, {})
-            # self.user_repo_events.setdefault(user, {})
-            # self.repo_events.setdefault(repo, {})
-            # self.user_repo_events[user].setdefault(repo, {})
-            #
-            # self.user_events[user][event] = self.user_events[user].get(event, 0)+1
-            # self.repo_events[repo][event] = self.repo_events[repo].get(event, 0)+1
-            # self.user_repo_events[user][repo][event] = self.user_repo_events[user][repo].get(event, 0)+1
+            asyncio.run(self.__read_files(cur_dir, filenames))
+            # for name in filenames:
+            #     self.__count_events(f'{cur_dir}/{name}')
 
         with open('0.json', 'wb') as f:
             pickle.dump(self.user_events, f)
@@ -105,15 +84,6 @@ class Data:
         with open('2.json', 'rb') as f:
             self.user_repo_events = pickle.load(f)
 
-    # def get_user_event(self, user: str, event: str) -> int:
-    #     return self.user_events.get(user, {}).get(event, 0)
-    #
-    # def get_repo_event(self, repo: str, event: str) -> int:
-    #     return self.repo_events.get(repo, {}).get(event, 0)
-    #
-    # def get_user_repo_event(self, user: str, repo: str, event: str) -> int:
-    #     return self.user_repo_events.get(user, {}).get(repo, {}).get(event, 0)
-
 
 class Run:
     def __init__(self):
@@ -130,7 +100,6 @@ class Run:
     def analyse(self):
         args = self.parser.parse_args()
 
-        # self.data = Data()
         if args.init:
             self.data.init(args.init)
             return 'init done'
